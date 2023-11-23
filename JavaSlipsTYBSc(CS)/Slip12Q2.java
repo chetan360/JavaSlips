21f32747f301d91858eb98a69959e43135f17538
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-11-23 16:44:44 +0530

    Update Slip12Q2.java

--- JavaSlipsTYBSc(CS)/Slip12Q2.java
+++ JavaSlipsTYBSc(CS)/Slip12Q2.java
@@ -1,102 +1,93 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-class Calculator extends JFrame implements ActionListener {
-    //J Components
-    private JTextField tField;
-    private JButton[] digitBtn;
-    private JButton addBtn, subBtn, mulBtn, divBtn, equalBtn, modBtn;
 
+
+class Calculator implements ActionListener {
+    private JFrame frame;
+    private JButton[] digitBtns;
+    private JButton addBtn, subBtn, mulBtn, divBtn, modBtn, equalBtn;
+    private JTextField tf;
+    
     private String input = "";
-    private double n1 = 0, n2 = 0;
-    private char operator;
-
-    public Calculator() {
-        setTitle("Simple Calculator.");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+    private double n1=0, n2=0;
+    private char op;
+    
+    Calculator1() {
+        frame = new JFrame("Calculatoer App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
         
-        //text field
-        tField = new JTextField();
-        tField.setEditable(false);
-        add(tField, BorderLayout.NORTH);
-
-        //Panel for btns 4x4
+        tf = new JTextField();
+        tf.setEditable(false);
+        frame.add(tf, BorderLayout.NORTH);
+        
         JPanel btnPanel = new JPanel();
         btnPanel.setLayout(new GridLayout(4, 4));
-
-        //adding digit btn to panel
-        digitBtn = new JButton[10];
+        
+        JButton[] digitBtn = new JButton[10];
         for(int i=0; i<10; i++) {
             digitBtn[i] = new JButton(String.valueOf(i));
             digitBtn[i].addActionListener(this);
             btnPanel.add(digitBtn[i]);
         }
-
-        //operation btns
+        
         addBtn = new JButton("+");
         subBtn = new JButton("-");
-        mulBtn = new JButton("*");
+        mulBtn = new JButton("x");
         divBtn = new JButton("/");
         modBtn = new JButton("%");
         equalBtn = new JButton("=");
-
-        JButton[] opButtons = {addBtn, subBtn, mulBtn, divBtn, modBtn, equalBtn};
-        for(JButton btn: opButtons) {
-            btn.addActionListener(this); //adding action to each btn
-            btnPanel.add(btn); //adding all btns one by one
+        
+        JButton[] opBtns = {addBtn, subBtn, mulBtn, divBtn, modBtn, equalBtn};
+        for(JButton x:opBtns) {
+            x.addActionListener(this);
+            btnPanel.add(x);
         }
-
-        add(btnPanel, BorderLayout.CENTER); //adding panel of btns to frame
-
-        pack(); //packs frame over all components
-        setLocationRelativeTo(null); //center the frame
-        setVisible(true); // make frame visible
+        
+        frame.add(btnPanel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
     }
-
-    @Override //override annotation actionPerformed
+    
+    @Override
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
-
+        
         if(Character.isDigit(cmd.charAt(0))) {
             input += cmd;
-            tField.setText(input);
-        } else if(cmd.charAt(0) == '+' || cmd.charAt(0) == '-' ||
-        cmd.charAt(0) == '*' || cmd.charAt(0) == '/' || cmd.charAt(0) == '%') {
-            n1 = Double.parseDouble(input);
-            operator = cmd.charAt(0);
+            tf.setText(input);
+        } else if(cmd.charAt(0) == '+' || cmd.charAt(0) == '-' || cmd.charAt(0) == 'x' ||
+        cmd.charAt(0) == '/' || cmd.charAt(0) == '%') {
+            n1 = Double.parseDouble(input);    
+            op = cmd.charAt(0);
             input = "";
         } else if(cmd.equals("=")) {
-            if(!input.isEmpty()) {
-                n2 = Double.parseDouble(input);
-                double res = perforOperation(n1, n2, operator);
-                tField.setText(String.valueOf(res));
-                input = "";
-            }
+            n2 = Double.parseDouble(input);
+            double result = performOperation(n1, n2, op);
+            tf.setText(String.valueOf(result));
+            input = "";
         }
     }
-
-    private double perforOperation(double n1, double n2, char operator) {
-        // opertion bassed on operators
-        switch (operator) {
-            case '+':
-                return n1 + n2;
-            case '-':
-                return n1 - n2;
-            case '*':
-                return n1 * n2;
-            case '/':
-                return n1 / n2;
-            case '%':
-                return n1 % n2;
-            default:
-                return 0;
+    
+    public double performOperation(double n1, double n2, char op) {
+        switch(op) {
+            case '+': return n1+n2;
+            case '-': return n1-n2;
+            case 'x': return n1*n2;
+            case '/': return n1/n2;
+            case '%': return n1%n2;
+            default: return 0;
         }
-    }
+    } 
 }
-
 public class Slip12Q2 {
     public static void main(String[] args) {
        new Calculator();
